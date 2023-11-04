542ee215c13e1b0a8191a24003e5ccc16a02463a
David Cheung <davidcheungtw@gmail.com>
2023-11-04 14:01:16 +0000

    add .env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDCSEuEoXk2dcyCBPcGIsm6hrS_OHfr97E",
-  authDomain: "fir-realtime-database-aaf4c.firebaseapp.com",
-  projectId: "fir-realtime-database-aaf4c",
-  storageBucket: "fir-realtime-database-aaf4c.appspot.com",
-  messagingSenderId: "451511025317",
-  appId: "1:451511025317:web:b607e1ec3b1a868ab7574c",
-};
+import firebaseConfig from "./config/firebase_config.json";
 
 const app = initializeApp(firebaseConfig);
 
